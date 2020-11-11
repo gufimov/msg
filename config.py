890731ef36f80f6dee553dba796bf18130ae435a
gufimov <72704694+gufimov@users.noreply.github.com>
2020-11-11 18:33:44 +0530

    Update and rename config_sample.py to config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # The bot-token which you can get from @Botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1468326058:AAGqmg4zrTxG2h18XVAC5AtEwTyq203VfEU")
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
-    AUTH_USERS = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "1086725567")
     # Add numbers below who shouldn't be bombed ever
-    NO_BOMB_NUMS = os.environ.get("NO_BOMB_NUMS", "")
+    NO_BOMB_NUMS = os.environ.get("NO_BOMB_NUMS", "8084534468")
     # Add userids below of users who should have sudo authority over bot, i.e., have no bombing limits
-    GOD_USERS = os.environ.get("GOD_USERS", "")
+    GOD_USERS = os.environ.get("GOD_USERS", "911646244")
